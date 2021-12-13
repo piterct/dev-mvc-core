a7105facc61014c042fb7fe18997f1d5d9580455
Michael Peter <michael_piterct@hotmail.com>
2021-12-12 21:44:27 -0300

    initial side client

--- scr/Dev.UI.Site/Startup.cs
+++ scr/Dev.UI.Site/Startup.cs
@@ -27,6 +27,8 @@ namespace Dev.UI.Site
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
