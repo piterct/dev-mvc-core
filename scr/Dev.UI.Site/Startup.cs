9f9508551c26809dff94fb26edd6f8f1e9db7d23
Michael Peter <michael_piterct@hotmail.com>
2021-12-13 22:59:19 -0300

    Dependecy injection

--- scr/Dev.UI.Site/Startup.cs
+++ scr/Dev.UI.Site/Startup.cs
@@ -1,3 +1,4 @@
+using Dev.UI.Site.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -27,6 +28,8 @@ namespace Dev.UI.Site
             });
             
             services.AddControllersWithViews();
+
+            services.AddTransient<IPedidoRepository, PedidoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
