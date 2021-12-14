2b4fb40148fef8b3107778f1eb0780686964f0da
Michael Peter <michael_piterct@hotmail.com>
2021-12-13 22:52:27 -0300

    routes for mvc areas

---
+++ scr/Dev.UI.Site/Models/Pedido.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Dev.UI.Site.Models
+{
+    public class Pedido
+    {
+        public Guid Id { get; set; }
+
+        public Pedido()
+        {
+            Id = new Guid();
+        }
+    }
+}
--- scr/Dev.UI.Site/Startup.cs
+++ scr/Dev.UI.Site/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -16,6 +17,15 @@ namespace Dev.UI.Site
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.AreaViewLocationFormats.Clear();
+                options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/{1}/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+
+            });
+            
             services.AddControllersWithViews();
         }
 
@@ -33,7 +43,10 @@ namespace Dev.UI.Site
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute("areas", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                //routes.MapRoute("areas", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapAreaControllerRoute("AreaProdutos","Produtos", "Produtos/{controller=Cadastro}/{action=Index}/{id?}");
+                endpoints.MapAreaControllerRoute("AreaVendas", "Vendas", "Vendas/{controller=Pedidos}/{action=Index}/{id?}");
 
                 endpoints.MapControllerRoute( "default", "{controller=Home}/{action=Index}/{id?}");
             });
