be625fc7d6c2c0cd3eec59941f0ef55c4265b740
Michael Peter <michael_piterct@hotmail.com>
2021-12-21 19:47:20 -0300

    startup configuration identity

---
+++ AppModelo/AspnetCoreIdentity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AspnetCoreIdentity.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AspnetCoreIdentity.Areas.Identity.IdentityHostingStartup))]
+namespace AspnetCoreIdentity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AspnetCoreIdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AspnetCoreIdentityContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AspnetCoreIdentityContext>();
+            });
+        }
+    }
+}
--- AppModelo/AspnetCoreIdentity/Startup.cs
+++ AppModelo/AspnetCoreIdentity/Startup.cs
@@ -1,6 +1,9 @@
+using AspnetCoreIdentity.Areas.Identity.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +27,13 @@ namespace AspnetCoreIdentity
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<AspnetCoreIdentityContext>(options =>
+                  options.UseSqlServer(Configuration.GetConnectionString("AspnetCoreIdentityContextConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddDefaultUI()
+                .AddEntityFrameworkStores<AspnetCoreIdentityContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +55,8 @@ namespace AspnetCoreIdentity
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
