7902be7bffaffba72ce1fee2191b537624631f2d
Michael Peter <michael_piterct@hotmail.com>
2021-12-29 22:09:49 -0300

    customize Razor claim

--- AppModelo/AspnetCoreIdentity/Extensions/CustomAuthorization.cs
+++ AppModelo/AspnetCoreIdentity/Extensions/CustomAuthorization.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
 using System;
 using System.Linq;
 using System.Security.Claims;
@@ -33,7 +34,15 @@ namespace AspnetCoreIdentity.Extensions
             }
             public void OnAuthorization(AuthorizationFilterContext context)
             {
-
+                if (!context.HttpContext.User.Identity.IsAuthenticated)
+                {
+                    context.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+                    {
+                        area = "Identity",
+                        page = "/Account/Login",
+                        ReturnUrl = context.HttpContext.Request.Path.ToString()
+                    }));
+                }
 
 
                 if (!ValidarClaimsUsuario(context.HttpContext, _claim.Type, _claim.Value))
