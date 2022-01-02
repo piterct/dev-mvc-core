0c7c8c2a880e0ee80ec3cce637c6884ddecae683
Michael Peter <michael_piterct@hotmail.com>
2022-01-02 20:47:02 -0300

    Endereco model

--- AppModelo/Dev.Mvc.Simples/Models/Endereco.cs
+++ AppModelo/Dev.Mvc.Simples/Models/Endereco.cs
@@ -1,6 +1,19 @@
-﻿namespace Dev.Mvc.Simples.Models
+﻿using System;
+
+namespace Dev.Mvc.Simples.Models
 {
     public class Endereco : Entity
     {
+        public Guid FornecedorId { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Cep { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+
+        /* Ef Relations */
+        public Fornecedor Fornecedor { get; set; }
     }
 }
