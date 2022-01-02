92413f83b929494cac5f270839cea76f82e6a2d6
Michael Peter <michael_piterct@hotmail.com>
2022-01-02 20:43:20 -0300

    produto model

--- AppModelo/Dev.Mvc.Simples/Models/Produto.cs
+++ AppModelo/Dev.Mvc.Simples/Models/Produto.cs
@@ -1,6 +1,19 @@
-﻿namespace Dev.Mvc.Simples.Models
+﻿using System;
+
+namespace Dev.Mvc.Simples.Models
 {
     public class Produto : Entity
     {
+        public Guid FornecedorId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Imagem { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        /* Ef Relations */
+
+        public Fornecedor Fornecedor { get; set; }
     }
 }
