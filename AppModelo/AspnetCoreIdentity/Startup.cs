baa5d6c8762bc4f2df3fad742b3d81186a824401
Michael Peter <michael_piterct@hotmail.com>
2022-01-02 04:34:41 -0300

    kind of errors page

--- AppModelo/AspnetCoreIdentity/Startup.cs
+++ AppModelo/AspnetCoreIdentity/Startup.cs
@@ -42,7 +42,8 @@ namespace AspnetCoreIdentity
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/erro/500");
+                app.UseStatusCodePagesWithRedirects("/erro/{0}");
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
