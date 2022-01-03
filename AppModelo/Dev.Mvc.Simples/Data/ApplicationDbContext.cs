6015e11a4fbc3d6b1335356cbb153d786a4eb0e3
Michael Peter <michael_piterct@hotmail.com>
2022-01-02 21:02:10 -0300

    data annotation models

--- AppModelo/Dev.Mvc.Simples/Data/ApplicationDbContext.cs
+++ AppModelo/Dev.Mvc.Simples/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Dev.Mvc.Simples.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,9 @@ namespace Dev.Mvc.Simples.Data
             : base(options)
         {
         }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
     }
 }
--- AppModelo/Dev.Mvc.Simples/Models/Fornecedor.cs
+++ AppModelo/Dev.Mvc.Simples/Models/Fornecedor.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Dev.Mvc.Simples.Models
 {
     public class Fornecedor : Entity
     {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(14, ErrorMessage = "O campo {0} ter entre {2} e {1} caracteres", MinimumLength = 11)]
         public string Documento { get; set; }
         public TipoFornecedor TipoFornecedor { get; set; }
         public Endereco Endereco { get; set; }
+        [DisplayName("Ativo?")]
         public bool Ativo { get; set; }
 
         /* Ef Relations */
